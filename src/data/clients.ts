feeb67f3598f76c281b5894798535dcb95de19a9
SOCOPWA <juan@socopwa.com>
2025-01-02 07:29:38 -0800

    Update clients.ts
    add proper logo links

--- src/data/clients.ts
+++ src/data/clients.ts
@@ -1,43 +1,43 @@
 export const clients = [
   {
     name: 'INFONAVIT',
-    logo: 'https://upload.wikimedia.org/wikipedia/commons/8/86/Logo_del_Infonavit.svg',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Logo_INFONAVIT.svg/2560px-Logo_INFONAVIT.svg.png',
     description: 'National Housing Fund Institute',
     descriptionEs: 'Instituto del Fondo Nacional de la Vivienda para los Trabajadores'
   },
   {
     name: 'CONADE',
-    logo: 'https://www.gob.mx/cms/uploads/article/main_image/83578/CONADE_ROJO.png',
+    logo: 'https://commons.wikimedia.org/wiki/File:Logo_CONADE.png',
     description: 'National Commission of Physical Culture and Sports',
     descriptionEs: 'Comisión Nacional de Cultura Física y Deporte'
   },
   {
     name: 'BX+',
-    logo: 'https://www.vepormas.com/web/wp-content/uploads/2020/11/logo-grupo-financiero-ve-por-mas.svg',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/2/24/Logo_del_Banco_Ve_por_M%C3%A1s.svg',
     description: 'Financial Services Group',
     descriptionEs: 'Grupo de Servicios Financieros'
   },
   {
     name: 'Compartamos Banco',
-    logo: 'https://www.compartamos.com.mx/compartamos/img/logo.svg',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/5/5a/Logo_de_Compartamos_Banco.svg',
     description: 'Microfinance Banking Institution',
     descriptionEs: 'Institución Bancaria de Microfinanzas'
   },
   {
     name: 'SARE',
-    logo: 'https://www.sare.mx/images/logo-sare.svg',
+    logo: 'https://coesmer.chiapas.gob.mx/images/2021/Logo_SARE.png',
     description: 'Construction and Real Estate Development',
     descriptionEs: 'Construcción y Desarrollo Inmobiliario'
   },
   {
     name: 'Sección Amarilla',
-    logo: 'https://seccionamarilla.com.mx/assets/img/logo/logo.svg',
+    logo: 'https://cdn.freebiesupply.com/logos/large/2x/seccion-amarilla-logo-png-transparent.png',
     description: 'Business Directory and Marketing Solutions',
     descriptionEs: 'Directorio Comercial y Soluciones de Marketing'
   },
   {
     name: 'Banco Multiva',
-    logo: 'https://www.multiva.com.mx/sites/default/files/logo-multiva.svg',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3b/Logo_de_Banco_Multiva.svg/2560px-Logo_de_Banco_Multiva.svg.png',
     description: 'Banking and Financial Services',
     descriptionEs: 'Servicios Bancarios y Financieros'
   }
