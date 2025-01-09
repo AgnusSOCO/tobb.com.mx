063013d430c97d8782ac604f5c8ccd7676307fda
SOCO GLOBAL <juan@socopwa.com>
2025-01-08 16:41:53 -0800

    Update index.js

--- src/data/articles/index.js
+++ src/data/articles/index.js
@@ -1,13 +1,13 @@
-import { Article } from '../../types/article';
+import type { Article } from '../../types/article';
 import { firescamArticle } from './firescam';
 import { miraiArticle } from './mirai';
 import { ukraineEspionageArticle } from './ukraine-espionage';
 
-export const articles: Article[] = [
+export const articles = [
   ukraineEspionageArticle, // Most recent first
   firescamArticle,
   miraiArticle,
-];
+] as const;
 
 export * from './firescam';
 export * from './mirai';
