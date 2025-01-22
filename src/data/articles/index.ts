14bc10c3f765dc3f776953dfc9769aa2f045b579
SOCO GLOBAL <juan@socopwa.com>
2025-01-22 11:17:11 -0800

    Update index.ts

--- src/data/articles/index.ts
+++ src/data/articles/index.ts
@@ -3,9 +3,11 @@ import { phishingGtldsArticle } from './phishing-gtlds';
 import { firescamArticle } from './firescam';
 import { miraiArticle } from './mirai';
 import { ukraineEspionageArticle } from './ukraine-espionage';
+import { silkRoadArticle } from './silk-road';
 
 export const articles: Article[] = [
-  phishingGtldsArticle, // Most recent first
+  silkRoadArticle, // Most recent first
+  phishingGtldsArticle,
   ukraineEspionageArticle,
   firescamArticle,
   miraiArticle,
@@ -15,3 +17,4 @@ export * from './phishing-gtlds';
 export * from './firescam';
 export * from './mirai';
 export * from './ukraine-espionage';
+export * from './silk-road';
